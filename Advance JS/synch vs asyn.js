c5535c3b2b70004bcae0f99ce5e0fad1b98222dc
rishavhack <rishav.jnit@gmail.com>
2021-09-21 12:37:41 +0530

    Synchronous vs Asynchronnous

--- Advance JS/synch vs asyn.js
+++ Advance JS/synch vs asyn.js
@@ -6,5 +6,9 @@ function getUSer(id) {
 	setTimeout(()=>{
 		console.log("Reading....")
 	return {id: id, githubUsername:'rishavhack'}
-	},2000);
+	},2000)	
 }
+//To solve this problem
+//1. Call back
+//2. await
+//3. Promise
