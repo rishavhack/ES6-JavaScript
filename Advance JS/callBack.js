0286c3a749ac2ffed1efca7f52cc9b332c40b8b4
rishavhack <rishav.jnit@gmail.com>
2021-09-23 12:52:40 +0530

    Call back using arrow functionn

--- Advance JS/callBack.js
+++ Advance JS/callBack.js
@@ -1,6 +1,8 @@
 console.log("Before");
 getUSer(1, function(user){
-	console.log('User',user);
+	getRepo(user.githubUsername,(repos)=>{
+		console.log('Repo',repos)
+	})
 });
 console.log('After')
 function getUSer(id,callback) {
@@ -9,3 +11,14 @@ function getUSer(id,callback) {
 		callback({id: id, githubUsername:'rishavhack'})
 	},2000)	
 }
+function getRepo(username,callback){
+	setTimeout(()=>{
+		console.log('Calling Github API...');
+		callback(['repo1','repo2','repo3'])
+	});
+}
+
+
+
+
+
